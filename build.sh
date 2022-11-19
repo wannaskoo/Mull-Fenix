26b7d33ee355a4ab354a9dc46c704042aa22d185
wannaskoo <wannaskoo@tutanota.com>
2022-11-19 19:25:15 +0100

    Revert "Unset MACH_BUILD_PYTHON_NATIVE_PACKAGE_SOURCE"
    This reverts commit df92162de2982b661bce2d25a6d038e741a097ea.

--- build.sh
+++ build.sh
@@ -73,6 +73,7 @@ make \
 popd
 
 pushd "$mozilla_release"
+export MACH_BUILD_PYTHON_NATIVE_PACKAGE_SOURCE=none
 ./mach --verbose build
 gradle publishWithGeckoBinariesReleasePublicationToMavenLocal
 gradle exoplayer2:publishReleasePublicationToMavenLocal
